43b5bd5f974cfe47a4f0e86f4f2152b89388f7f5
G.magpie <msk7529@naver.com>
2021-07-03 01:31:16 +0900

    Chapter2. Publishers & Subscribers - 4 > assign operator

--- CombineStudy/CombineStudy/Chapters/Chapter02_ViewController.swift
+++ CombineStudy/CombineStudy/Chapters/Chapter02_ViewController.swift
@@ -28,6 +28,18 @@ final class Chapter02_ViewController: UIViewController {
         return button
     }()
     
+    private lazy var fourthExBtn: commonBtn = {
+        let button: commonBtn = .init(title: "fourthEx")
+        button.addTarget(self, action: #selector(didTapFourthEx), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var fifthExBtn: commonBtn = {
+        let button: commonBtn = .init(title: "fifthExBtn")
+        button.addTarget(self, action: #selector(didTapFifthEx), for: .touchUpInside)
+        return button
+    }()
+    
     private let notiName: Notification.Name = .init("MyNotification")
     
     override func viewDidLoad() {
@@ -43,6 +55,8 @@ final class Chapter02_ViewController: UIViewController {
         self.view.addSubview(firstExBtn)
         self.view.addSubview(secondExBtn)
         self.view.addSubview(thirdExBtn)
+        self.view.addSubview(fourthExBtn)
+        self.view.addSubview(fifthExBtn)
         
         firstExBtn.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
         firstExBtn.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 30).isActive = true
@@ -58,6 +72,16 @@ final class Chapter02_ViewController: UIViewController {
         thirdExBtn.leadingAnchor.constraint(equalTo: secondExBtn.trailingAnchor, constant: 30).isActive = true
         thirdExBtn.widthAnchor.constraint(equalToConstant: 100).isActive = true
         thirdExBtn.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        fourthExBtn.topAnchor.constraint(equalTo: firstExBtn.bottomAnchor, constant: 30).isActive = true
+        fourthExBtn.leadingAnchor.constraint(equalTo: firstExBtn.leadingAnchor).isActive = true
+        fourthExBtn.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        fourthExBtn.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        fifthExBtn.topAnchor.constraint(equalTo: fourthExBtn.topAnchor).isActive = true
+        fifthExBtn.leadingAnchor.constraint(equalTo: fourthExBtn.trailingAnchor, constant: 30).isActive = true
+        fifthExBtn.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        fifthExBtn.heightAnchor.constraint(equalToConstant: 30).isActive = true
     }
 }
 
@@ -94,13 +118,65 @@ extension Chapter02_ViewController {
     
     private func justTest1() {
         example(of: "Just") {
+            // Just: 각각의 subscriber에 대해 단 한개의 출력을 emit하는 Publisher.
             let just: Just<String> = .init("Hello World!")
             
+            // sink operator는 publisher가 방출한 모든 값들을 계속 받을 수 있다.
+            // receiveValue 클로저가 먼저 호출되고, 그 다음 receiveCompletion 클로저가 호출된다.
             _ = just.sink(receiveCompletion: { failure in
                 print("Received completion", failure)
             }, receiveValue: {
                 print("Received value", $0)
             })
+        }
+    }
+    
+    private func subscribingWithAssign() {
+        example(of: "assign(to:on:)") {
+            class SomeObject {
+                var value: String = "" {
+                    didSet {
+                        print(value)
+                    }
+                }
+            }
+            
+            let object: SomeObject = .init()
+            let publisher: Publishers.Sequence<[String], Never> = ["Hello", "world!"].publisher
+            
+            // received value를 객체의 KVO에 상응하는 property에 assign 한다.
+            _ = publisher.assign(to: \.value, on: object)
+        }
+    }
+    
+    private func republishingWithAssign() {
+        example(of: "assign(to:)") {
+            class SomeObject {
+                @Published var value = 0    // value에 대한 publisher를 생성하며, 일반적인 Property로도 사용할 수 있다.
+            }
+            
+            let object: SomeObject = .init()
+            
+            _ = object.$value.sink { print($0) }    // 0
+            
+            // assign(to:) operator는 AnyCancellable을 리턴하지 않는다.
+            // 이유는 오퍼레이터가 내부적으로 라이프사이클을 관리하며 @Published property가 메모리에서 해제되면 subscription을 취소시키기 때문.
+            (0..<10).publisher.assign(to: &object.$value)
+            
+            print(object.value) // 9
+            
+            // assign(to:)가 assign(to:on:)에 비해 이점을 가지는 경우는, 아래와 같이 전자는 강한참조로 인한 메모리 이슈를 걱정할 필요가 없다.
+            class MyObject {
+                @Published var word: String = ""
+                var subscriptions: Set<AnyCancellable> = .init()
+                
+                init() {
+                    ["A", "B", "C"].publisher
+                        .assign(to: \.word, on: self)
+                        .store(in: &subscriptions)
+                }
+            }
+            
         }
     }
 }
@@ -117,5 +193,13 @@ extension Chapter02_ViewController {
     
     @objc private func didTapThirdEx() {
         justTest1()
+    }
+    
+    @objc private func didTapFourthEx() {
+        subscribingWithAssign()
+    }
+    
+    @objc private func didTapFifthEx() {
+        republishingWithAssign()
     }
 }
