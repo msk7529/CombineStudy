613bfaf43235c059b7edaa1e591482bd98fca090
G.magpie <msk7529@naver.com>
2021-07-11 01:05:30 +0900

    Chapter9. Networking - 2 > Codable support

--- CombineStudy/CombineStudy/Chapters/Chapter09_ViewController.swift
+++ CombineStudy/CombineStudy/Chapters/Chapter09_ViewController.swift
@@ -10,9 +10,31 @@ import Combine
 
 final class Chapter09_ViewController: UIViewController {
     
+    struct TestModel: Codable {
+        let name: String
+        let age: Int
+        
+        private enum CodingKeys: String, CodingKey {
+            case name
+            case age = "Age"
+        }
+        
+        init(from decoder: Decoder) throws {
+            let values = try decoder.container(keyedBy: CodingKeys.self)
+            name = (try? values.decode(String.self, forKey: .name)) ?? ""
+            age = (try? values.decode(Int.self, forKey: .age)) ?? 0
+        }
+    }
+    
     private lazy var firstExBtn: commonBtn = {
         let button: commonBtn = .init(title: "firstEx")
         button.addTarget(self, action: #selector(didTapFirstEx), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var secondExBtn: commonBtn = {
+        let button: commonBtn = .init(title: "secondEx")
+        button.addTarget(self, action: #selector(didTapSecondEx), for: .touchUpInside)
         return button
     }()
     
@@ -29,11 +51,17 @@ final class Chapter09_ViewController: UIViewController {
     
     private func initView() {
         self.view.addSubview(firstExBtn)
+        self.view.addSubview(secondExBtn)
         
         firstExBtn.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
         firstExBtn.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 30).isActive = true
         firstExBtn.widthAnchor.constraint(equalToConstant: 100).isActive = true
         firstExBtn.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        secondExBtn.topAnchor.constraint(equalTo: firstExBtn.topAnchor).isActive = true
+        secondExBtn.leadingAnchor.constraint(equalTo: firstExBtn.trailingAnchor, constant: 30).isActive = true
+        secondExBtn.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        secondExBtn.heightAnchor.constraint(equalToConstant: 30).isActive = true
     }
 }
 
@@ -41,20 +69,44 @@ final class Chapter09_ViewController: UIViewController {
 extension Chapter09_ViewController {
     
     private func networking() {
-        guard let url = URL(string: "https://www.naver.com") else {
-            return
+        example(of: "URLSession extensions") {
+            guard let url = URL(string: "https://www.naver.com") else {
+                return
+            }
+            
+            URLSession.shared
+                .dataTaskPublisher(for: url)
+                .sink(receiveCompletion: { completion in
+                    if case .failure(let error) = completion {
+                        print("Retrieving data failed with error \(error)")
+                    }
+                }, receiveValue: { data, response in
+                    print("Retrieved data of size \(data.count), response = \(response)")
+                })
+                .store(in: &subscriptions)
         }
-        
-        URLSession.shared
-            .dataTaskPublisher(for: url)
-            .sink(receiveCompletion: { completion in
-                if case .failure(let error) = completion {
-                    print("Retrieving data failed with error \(error)")
-                }
-            }, receiveValue: { data, response in
-                print("Retrieved data of size \(data.count), response = \(response)")
-            })
-            .store(in: &subscriptions)
+    }
+    
+    private func codableSupport() {
+        example(of: "Codable Support") {
+            guard let url = URL(string: "https://www.naver.com") else {
+                return
+            }
+            
+            URLSession.shared
+                .dataTaskPublisher(for: url)
+                .print("codable Test: ")
+                .map(\.data)
+                .decode(type: TestModel.self, decoder: JSONDecoder())
+                .sink(receiveCompletion: { completion in
+                    if case .failure(let error) = completion {
+                        print("Retrieving data failed with error \(error)")
+                    }
+                }, receiveValue: { object in
+                    print("Retrieved object \(object)")
+                })
+                .store(in: &subscriptions)
+        }
     }
 }
 
@@ -63,5 +115,9 @@ extension Chapter09_ViewController {
     
     @objc private func didTapFirstEx() {
         networking()
+    }
+    
+    @objc private func didTapSecondEx() {
+        codableSupport()
     }
 }
