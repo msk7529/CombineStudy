308cd8a33acfd82f7e98c8254e18830a7bbf3298
G.magpie <msk7529@naver.com>
2021-07-12 03:01:08 +0900

    Chapter13. Resource Management - 2 > The multicast(_:) operator

--- CombineStudy/CombineStudy/Chapters/Chapter13_ViewController.swift
+++ CombineStudy/CombineStudy/Chapters/Chapter13_ViewController.swift
@@ -16,6 +16,12 @@ final class Chapter13_ViewController: UIViewController {
         return button
     }()
     
+    private lazy var secondExBtn: commonBtn = {
+        let button: commonBtn = .init(title: "secondEx")
+        button.addTarget(self, action: #selector(didTapSecondEx), for: .touchUpInside)
+        return button
+    }()
+    
     private var subscriptions: Set<AnyCancellable> = .init()
     
     override func viewDidLoad() {
@@ -29,11 +35,17 @@ final class Chapter13_ViewController: UIViewController {
 
     private func initView() {
         self.view.addSubview(firstExBtn)
+        self.view.addSubview(secondExBtn)
         
         firstExBtn.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
         firstExBtn.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 30).isActive = true
         firstExBtn.widthAnchor.constraint(equalToConstant: 100).isActive = true
         firstExBtn.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        secondExBtn.topAnchor.constraint(equalTo: firstExBtn.topAnchor).isActive = true
+        secondExBtn.leadingAnchor.constraint(equalTo: firstExBtn.trailingAnchor, constant: 30).isActive = true
+        secondExBtn.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        secondExBtn.heightAnchor.constraint(equalToConstant: 30).isActive = true
     }
 }
 
@@ -74,6 +86,41 @@ extension Chapter13_ViewController {
             }
         }
     }
+    
+    private func multicast() {
+        // connect()를 호출해야 이벤트를 시작한다.
+        // 따라서, subscription들을 다 만들어두고, connect를 할 수 있어 편리하다.
+        example(of: "multicast") {
+            let subject = PassthroughSubject<Data, URLError>()  // upstream publisher가 내보내는 값과 완료이벤트를 전달하는 subject
+            
+            let multicasted = URLSession.shared
+                .dataTaskPublisher(for: URL(string: "https://www.raywenderlich.com")!)
+                .map(\.data)
+                .print("multicast")
+                .multicast(subject: subject)
+            
+            multicasted
+                .sink(receiveCompletion: { _ in }, receiveValue: { print("subscription1 received \($0)")})
+                .store(in: &subscriptions)
+            
+            multicasted
+                .sink(receiveCompletion: { _ in }, receiveValue: { print("subscription2 received \($0)")})
+                .store(in: &subscriptions)
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) { [weak self] in
+                guard let strongSelf = self else { return }
+                
+                multicasted
+                    .sink(receiveCompletion: { print("subscription3 completion \($0)") },
+                          receiveValue: { print("subscription3 received \($0)") })
+                    // 이 시점에는 이미 publihser가 완료된 상태여서 value를 받을 수 없다.
+                    .store(in: &strongSelf.subscriptions)
+                
+                multicasted.connect()   // publisher에게 upstream publisher와 연결하도록 한다.
+                    .store(in: &strongSelf.subscriptions)
+            }
+        }
+    }
 }
 
 // - MARK: Button Actions
@@ -81,5 +128,9 @@ extension Chapter13_ViewController {
     
     @objc private func didTapFirstEx() {
         share()
+    }
+    
+    @objc private func didTapSecondEx() {
+        multicast()
     }
 }
